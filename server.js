71c233b6c568cdaac5820bd2325124f20ab5723c
fgarbagnati <f.g.garbagnati@gmail.com>
2014-09-09 00:32:50 -0700

    deleted blank line

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.listen( port, function() {
   console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
 
-
 mongoose.connect( 'mongodb://localhost/twitter-clone-database' );
 
 // Schemas
